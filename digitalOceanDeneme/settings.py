2037cc15e3d8ac680b163136a23d0f3c0b1d353c
unknown <berkelmas96@gmail.com>
2019-03-23 13:45:46 +0300

    databse ayarları yapıldı.

--- digitalOceanDeneme/settings.py
+++ digitalOceanDeneme/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'digitalOceanDeneme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'denemedatabase',
+        'USER': 'denemedatabaseuser',
+        'PASSWORD': 'berk693693693',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
